bd66cca9855ccd9be305b13de59c5a1944e74d4c
jamsheed saeed <jamsheed saeed>
2018-05-04 21:52:56 +0500

    start added to package.json

--- models/leave.js
+++ models/leave.js
@@ -1,5 +1,16 @@
+import { Stream } from 'stream';
+
 var mongoose = require('mongoose');
 var leaveSchema = new mongoose.Schema({
-    leave:String
+   // leave:String
+
+   name:String,
+   designation:String,
+   leavetype:String,
+   fromdate:String,
+   todate:String,
+   nodays:String,
+   reason:String
+
 });
 const leave = module.exports = mongoose.model('LeaveSchema', leaveSchema);
