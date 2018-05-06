9f2efedbd9805782c04c9ffc36d6fdfc9e2074d2
jamsheed saeed <jamsheed saeed>
2018-05-06 13:56:38 +0500

    start added to package.json

--- config/database.js
+++ config/database.js
@@ -1,6 +1,10 @@
 module.exports = {
     
-        'url' : 'mongodb://jamsheed saeed:malik1234@ds161574.mlab.com:61574/new' 
+        //'url' : 'mongodb://jamsheed saeed:malik1234@ds161574.mlab.com:61574/new' 
+
+        'url' : 'mongodb://jamsheed:malik1234@ds215910.mlab.com:15910/ems' 
+
+        
        
     };
    
